48d547ab345510d056a112b006ce801ea30625cd
moonkey48 <moonkey48@icloud.com>
2022-07-17 16:20:20 +0900

    3주차 개발

---
+++ testApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class AiClass(models.Model):
+    class_num = models.IntegerField()
+    teacher = models.CharField(max_length=30)
+    class_room = models.CharField(max_length=30)
+    students_num = models.IntegerField()
+
+
+class AiStudent(models.Model):
+    class_num = models.IntegerField()
+    name = models.CharField(max_length=30)
+    phone_num = models.CharField(max_length=30)
+    intro_text = models.TextField()
---
+++ testApp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .models import AiClass, AiStudent
+
+# Create your views here.
+
+
+def home(request):
+    class_object = AiClass.objects.all()
+    return render(request, 'test/home.html', {'class_object': class_object})
+
+
+def result(request):
+    name = request.POST['username']
+    students = ['inu', 'james', 'bob']
+
+    if name in students:
+        is_exist = True
+    else:
+        is_exist = False
+
+    return render(request, 'test/result.html', {'user_name': name, 'is_exist': is_exist})
