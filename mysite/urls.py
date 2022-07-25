618c9cc8ff460a271a19cbdd6d4931241c279655
moonkey48 <moonkey48@icloud.com>
2022-07-25 20:29:38 +0900

    4주차 개발(댓글 기능)

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('<int:content_id>/', views.detail, name='detail'),
     path('collection/', views.collection, name='collection'),
     path('additem/', views.additem, name='additem'),
+    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
+    path('comment/update/<int:comment_id>/', views.comment_update, name='comment_update'),
+    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import MainContent
+from .models import MainContent, Comment
+from .forms import CommentForm
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -28,4 +31,50 @@ def additem(request):
     return redirect('collection')
 
 
+@login_required(login_url='accounts:login')
+def comment_create(request, content_id):
+    content_list = get_object_or_404(MainContent, pk=content_id)
 
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.content_list = content_list
+            comment.author = request.user
+            comment.save()
+            return redirect('detail', content_id=content_list.id)
+    else:
+        form = CommentForm()
+    context = {'content_list': content_list, 'form': form}
+    return render(request, 'mysite/content_detail.html', context)
+
+
+@login_required(login_url='accounts:login')
+def comment_update(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.user != comment.author:
+        raise PermissionDenied
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('detail', content_id=comment.content_list.id)
+    else:
+        form = CommentForm(instance=comment)
+
+    context = {'comment': comment, 'form': form}
+    return render(request, 'mysite/comment_form.html', context)
+
+
+@login_required(login_url='accounts:login')
+def comment_delete(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if request.user != comment.author:
+        raise PermissionDenied
+    else:
+        comment.delete()
+    return redirect('detail', content_id=comment.content_list.id)
